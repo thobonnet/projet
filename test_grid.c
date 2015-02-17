59dddcfe7c8af31533c8d7a89b51e7a35d2b6924
Jeremy Jajoux <jeremy.jajoux@etu.u-bordeaux.fr>
2015-02-17 09:30:29 +0100

    Commit 15

--- test_grid.c
+++ test_grid.c
@@ -2,7 +2,19 @@
 #include <stdlib.h>
 #include "grid.h"
 
-int main(){
-  printf("Hello \n");
-  return EXIT_SUCCESS;
+void
+afficher_grille(grid g){
+  for(int x = 0; x < GRID_SIDE; x++){
+    for(int y = 0; y < GRID_SIDE; y++){
+      printf("%d ", g->cells[x][y]);
+    }
+    printf("\n");
+  }
+}
+
+int
+main(int argc, char **argv){
+  grid g = new_grid();
+  afficher_grille(g);
+	return EXIT_SUCCESS;
 }
