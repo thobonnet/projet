ee8271d12091fdeddcdac5923fee8ccf3d48c53c
Thomas Bonnet <thomas.bonnet@etu.u-bordeaux.fr>
2015-02-17 10:24:21 +0100

    change grid.c

--- grid.c
+++ grid.c
@@ -4,11 +4,12 @@
 #include "grid.h"
 
 struct grid_s{
-  int score;
+  unsigned long int score;
   tile** cells;
 };
 
-grid new_grid (){
+grid
+new_grid (){
   grid g=malloc(sizeof(struct grid_s));
   assert(g!=NULL);
   g->score=0;
@@ -23,16 +24,35 @@ grid new_grid (){
   return g;
 }
     
-void delete_grid (grid g){
+void
+delete_grid (grid g){
   for(int x=0;x<GRID_SIDE;x++)
     free(g->cells[x]);
   free(g->cells);
   free(g);
 }
 
-tile get_tile (grid g, int x, int y){
+void
+copy_grid (grid src, grid dst){
+  grid dst=new_grid();
+  dst->score=src->score;
+  for(int x=0;x<GRID_SIDE;x++)
+    for(int y=0;y<GRID_SIDE;y++)
+      dst->cells[x][y]=src->cells[x][y];
+}
+
+unsigned long int
+grid_score (grid g){
+  return g->score;
+}
+
+tile
+get_tile (grid g, int x, int y){
   assert((x>=0 && x<GRID_SIDE) && (y>=0 && y<GRID_SIDE));
   return g->cells[x][y];
+}
+
+void set_tile (grid g, int x, int y, tile t){
 }
 
 /*void copy_grid (grid src, grid dst);
