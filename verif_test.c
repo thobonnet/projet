45253663362111f7fcaaaa5305c5ac999f17af58
Jeremy Jajoux <jaeremy.jajoux@etu.u-bordeaux.fr>
2015-04-09 14:23:21 +0200

    verif_test terminé

--- verif_test.c
+++ verif_test.c
@@ -6,93 +6,113 @@
 #include <assert.h>
 #include "grid.h"
 
-bool
-test_new_grid(){
-	grid g = new_grid();
+void
+afficher_grille(grid g){
+  for(int i = 0; i < GRID_SIDE; i++){
+    printf("+------");
+  }
+  printf("+\n");
 
-	return g != NULL;
-}
-
-bool
-test_delete_grid(grid g){
-	delete_grid(g);
-
-	return g == NULL;
-}
-
-bool
-test_copy_grid(grid src, grid dst){
-	copy_grid(src, dst);
-	for(int x=0;x<GRID_SIDE;x++)
-    	for(int y=0;y<GRID_SIDE;y++)
-    		if(get_tile(src, x, y) != get_tile(dst, x, y))
-    			return false;
-
-    return true;
-}
-
-bool
-test_add_tile(){
-	int n = 0;
-	grid g = new_grid(); // Doit etre testé sur une grille vide
-	add_tile(g);
-	for(int x=0;x<GRID_SIDE;x++)
-    	for(int y=0;y<GRID_SIDE;y++)
-    		if(get_tile(g, x, y) != 0)
-    			n+=1;
-
-    return n == 1;
-}
-
-bool
-test_can_move(grid g){
-	tile t = 1;
-	for(int x=1;x>GRID_SIDE;x++)
-		for (int y=1;y<GRID_SIDE;y++)
-			set_tile(g,x,y,t++); // Remplissage de la grille de façon a autoriser seulement 2 direction
-
-	return can_move(g, DOWN) && can_move(g, RIGHT) 
-		&& !can_move(g, UP) && !can_move(g, LEFT);
-}
-
-bool
-test_game_over(grid g){
-	tile t = 1;
-	for(int x=0;x<GRID_SIDE;x++)
-		for(int y=0;y<GRID_SIDE;y++)
-			set_tile(g,x,y,t++); // Remplissage de la grille pour eviter tout mouvement possible.
-
-	return game_over(g);
-}
-
-bool
-test_play(grid g, dir d){
-	return test_add_tile();
-}
+  for(int x = 0; x < GRID_SIDE; x++){
+    printf("|");
+    for(int y = 0; y < GRID_SIDE; y++)
+      printf(" %4d |", get_tile(g,x,y)==0?0:(int)pow(2,get_tile(g,x,y)));
+    printf("\n");
+    for(int i = 0; i < GRID_SIDE; i++)
+      printf("+------");
+    printf("+\n");
+  }
+ }
 
 int
 main(int argc, char **argv){
 
-	printf("Test new_grid      : %d \n", test_new_grid());
+	grid g = new_grid();
+	printf("Affichage de la grille : \n");
+	afficher_grille(g);
 
-	grid tmp = new_grid();
-	printf("Test delete_grid   : %d \n", test_delete_grid(tmp));
+	printf("Test add_tile       : \n");
+	add_tile(g);
+	afficher_grille(g);
 
-	grid src = new_grid();
-	grid dst = new_grid();
-	printf("Test copy_grid     : %d \n", test_copy_grid(src, dst));
+	printf("Test can_move       : \n");
+	printf("Remplissage de la grille pour autoriser des deplacements\n");
+	printf("vers la gauche et vers le haut \n");
+	tile t = 1;
+	for(int x=1;x<GRID_SIDE;x++){
+		for (int y=1;y<GRID_SIDE;y++){
+			set_tile(g,x,y,t);
+			t++;
+		}
+	}
 
+	afficher_grille(g);
 
-	printf("Test add_tile      : %d \n",test_add_tile());
+	printf("Test can_move(UP)    : %d \n", can_move(g, UP));
+	printf("Test can_move(LEFT)  : %d \n", can_move(g, LEFT));
+	printf("Test can_move(DOWN)  : %d \n", can_move(g, DOWN));
+	printf("Test can_move(RIGHT) : %d \n", can_move(g, RIGHT));
+	
+	printf("Test do_move         : \n");
+	afficher_grille(g);
+	do_move(g, UP);
+	afficher_grille(g);
+	if(!can_move(g,UP)){
+		printf("Test do_move(UP) : OK \n\n");
+	}else{
+		printf("Test do_move(UP) : NOT OK\n\n");
+	}
 
-	printf("Test can_move      : %d \n",test_can_move(dst));
+	do_move(g, LEFT);
+	afficher_grille(g);
+	if(!can_move(g,LEFT)){
+		printf("Test do_move(LEFT) : OK \n\n");
+	}else{
+		printf("Test do_move(LEFT) : NOT OK\n\n");
+	}
 
-	printf("Test game_over     : %d \n",test_game_over(dst));
+	do_move(g, DOWN);
+	afficher_grille(g);
+	if(!can_move(g,DOWN)){
+		printf("Test do_move(DOWN) : OK \n\n");
+	}else{
+		printf("Test do_move(DOWN) : NOT OK\n\n");
+	}
 
-	printf("Test play          : %d \n",test_play(dst, DOWN));
+	do_move(g, RIGHT);
+	afficher_grille(g);
+	if(!can_move(g,RIGHT)){
+		printf("Test do_move(RIGHT) : OK \n\n");
+	}else{
+		printf("Test do_move(RIGHT) : NOT OK\n\n");
+	}
 
-	delete_grid(dst);
-	delete_grid(src);
+	printf("Test copy_grid : Les deux grille doivent être identique \n");
+	grid g2 = new_grid();
+	copy_grid(g,g2);
+	afficher_grille(g);
+	afficher_grille(g2);
 
+	bool b = true;
+	for(int x=0;x<GRID_SIDE;x++)
+    	for(int y=0;y<GRID_SIDE;y++)
+    		if(get_tile(g, x, y) != get_tile(g2, x, y))
+    			b = false;
+
+    printf("Test copy_grid : %d \n", b);
+
+    t = 1;
+    for(int x=0;x<GRID_SIDE;x++){
+		for (int y=0;y<GRID_SIDE;y++){
+			set_tile(g,x,y,t);
+			t++;
+		}
+	}
+	printf("Test game_over : Aucune direction possible \n");
+	afficher_grille(g);
+	printf("test_game_over : %d \n", game_over(g));
+
+	delete_grid(g);
+	delete_grid(g2);
 }
 
